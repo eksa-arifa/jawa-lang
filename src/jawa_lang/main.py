6098e3cde8eb2885af6d362c5d3750f1c3b9f730
eksa-arifa <eksaarifa@gmail.com>
2026-05-16 13:53:40 +0700

    chore: setup pip deploy

---
+++ src/jawa_lang/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+import os
+from jawa_lang.logic.parseSyntax import parseSyntax
+
+def jawaInterpreter():
+    if len(sys.argv) < 2:
+        print("Penggunaan: jawa <file.jawa>")
+        return
+
+    locateFile = sys.argv[1]
+
+    if not locateFile.endswith(".jawa"):
+        print(locateFile + " " + "Bukan jawa language")
+        return
+
+    if not os.path.exists(locateFile):
+        print(f"File {locateFile} ora ditemokake")
+        return
+
+    with open(locateFile, 'r') as f:
+        fread = f.read()
+
+    h = fread.split('\n')
+    result = map(parseSyntax, h)
+    res = list(result)
+
+    ctoe = ""
+    for r in res:
+        ctoe += r + '\n'
+    
+    os.system("python3 -c '"+ctoe+"'")
+
+if __name__ == "__main__":
+    jawaInterpreter()
